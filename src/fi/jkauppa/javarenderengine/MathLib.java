ee81efb7d5bda42b467362f043525bb94215bdb8
goofyseeker311 <goofyseeker311@gmail.com>
2023-12-16 14:57:25 +0200

    updated MathLib
    updated generatePolygonList.

--- src/fi/jkauppa/javarenderengine/MathLib.java
+++ src/fi/jkauppa/javarenderengine/MathLib.java
@@ -379,15 +379,15 @@ public class MathLib {
 	public static Polyangle[] generatePolygonList(Position2[] linelist) {
 		Position[] vertexlist = generateVertexList(linelist);
 		ArrayList<Polyangle> uniquepolygonlist = new ArrayList<Polyangle>();
-		for (int i=0;i<linelist.length;i++) {
-			if (linelist[i].pos1.compareTo(linelist[i].pos2)==0) {
+		for (int j=0;j<linelist.length;j++) {
+			if (linelist[j].pos1.compareTo(linelist[j].pos2)==0) {
 				Position[] linevertexlist = new Position[1];
-				linevertexlist[0] = linelist[i].pos1;
+				linevertexlist[0] = linelist[j].pos1;
 				uniquepolygonlist.add(new Polyangle(linevertexlist));
 			} else {
 				Position[] linevertexlist = new Position[2];
-				linevertexlist[0] = linelist[i].pos1;
-				linevertexlist[1] = linelist[i].pos2;
+				linevertexlist[0] = linelist[j].pos1;
+				linevertexlist[1] = linelist[j].pos2;
 				uniquepolygonlist.add(new Polyangle(linevertexlist));
 			}
 		}
