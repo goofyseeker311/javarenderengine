09eb497153224cc9039b47ca0a84c5e021d29ce6
goofyseeker311 <goofyseeker311@gmail.com>
2023-12-27 01:47:26 +0200

    Updated GameApp
    Added clipped textured polygon drawing instead of flat color filled polygon in GameApp (not yet aligned with texture coordinates).
    Added loadImage for loading images UtilLib (similar to loadWaveFrontOBJFile in ModelLib).

--- src/fi/jkauppa/javarenderengine/UtilLib.java
+++ src/fi/jkauppa/javarenderengine/UtilLib.java
@@ -1,10 +1,25 @@
 package fi.jkauppa.javarenderengine;
 
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+import java.awt.image.VolatileImage;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.filechooser.FileFilter;
 
 public class UtilLib {
+	private static GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment ();
+	private static GraphicsDevice gd = ge.getDefaultScreenDevice ();
+	private static GraphicsConfiguration gc = gd.getDefaultConfiguration ();
+	
 	public static class ImageFileFilters  {
 		public static class PNGFileFilter extends FileFilter {
 			@Override public boolean accept(File f) {return (f.isDirectory())||(f.getName().endsWith(".png"));}
@@ -32,5 +47,30 @@ public class UtilLib {
 			@Override public boolean accept(File f) {return (f.isDirectory())||(f.getName().endsWith(".obj"));}
 			@Override public String getDescription() {return "OBJ Model file";}
 		}
+	}
+	public static VolatileImage loadImage(String filename, boolean loadresourcefromjar) {
+		VolatileImage k = null;
+		if (filename!=null) {
+			try {
+				File imagefile = new File(filename);
+				BufferedInputStream imagefilestream = null;
+				if (loadresourcefromjar) {
+					imagefilestream = new BufferedInputStream(ClassLoader.getSystemClassLoader().getResourceAsStream(imagefile.getPath().replace(File.separatorChar, '/')));
+				}else {
+					imagefilestream = new BufferedInputStream(new FileInputStream(imagefile));
+				}
+				BufferedImage loadimage = ImageIO.read(imagefilestream);
+				if (loadimage!=null) {
+					VolatileImage loadimagevolatile = gc.createCompatibleVolatileImage(loadimage.getWidth(), loadimage.getHeight(), Transparency.TRANSLUCENT);
+					Graphics2D loadimagevolatilegfx = loadimagevolatile.createGraphics();
+					loadimagevolatilegfx.setComposite(AlphaComposite.Src);
+					loadimagevolatilegfx.drawImage(loadimage, 0, 0, null);
+					loadimagevolatilegfx.dispose();
+					k = loadimagevolatile;
+				}
+				imagefilestream.close();
+			} catch (Exception ex) {ex.printStackTrace();}
+		}
+		return k;
 	}
 }
